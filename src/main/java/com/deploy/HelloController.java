aee102fa70b7526d76ee319745ecf7cb9b97624d
kimsungho <tigertjdgh@gmail.com>
2023-08-29 20:49:59 +0900

    feat:  인덱스 컨트롤러 추가

--- src/main/java/com/deploy/HelloController.java
+++ src/main/java/com/deploy/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "안녕하세요 반갑습니다";
+    }
 
     @GetMapping("/hello")
     public String hello() {
